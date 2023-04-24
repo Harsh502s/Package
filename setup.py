000956cdacfc88ddcd80563f0eeb4fa6b513c6e5
Harshit Singh <harsh502singh@gmail.com>
2023-04-24 15:45:43 +0530

    Update

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="Astras",
     version="0.1",
-    packages=setup.find_packages(),
+    packages=find_packages(),
     install_requires=[
         "pandas",
         "numpy",
@@ -15,12 +15,12 @@ setup(
     ],
     author="Harshit Singh",
     author_email="harsh502singh@gmail.com",
-    url='https://github.com/Harsh502s/Astras',
+    url="https://github.com/Harsh502s/Astras",
     description="Package for Classification and Regression",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10"
-    )
+    python_requires=">=3.10",
+)
